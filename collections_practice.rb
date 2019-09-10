dc49facde3da0af717fe9618db126ad88089116e
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-10 23:16:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,14 +6,3 @@ def sort_array_desc(array)
   array.sort.reverse
 end
 
-def sort_array-char_count(array)
-  array.sort do |a, b|
-    if a.length == b.length
-      0
-    elsif a.length < b.length
-      -1
-    elsif a.length > b.length
-      1
-    end
-  end
-end
